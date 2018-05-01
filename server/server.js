d5b7b54fc0bf17c4c78954af472300bbd0bea5fd
Tyler <tylerlewisjcg@gmail.com>
2018-04-30 21:02:30 -0600

    Server setup

--- server/server.js
+++ server/server.js
@@ -5,9 +5,7 @@ const express = require("express"),
   massive = require("massive"),
   cors = require("cors"),
   passport = require("passport"),
-  Auth0Strategy = require("passport-auth0"),
-  S3 = require("./s3");
-const path = require("path");
+  Auth0Strategy = require("passport-auth0");
 const {
   SERVER_PORT,
   SESSION_SECRET,
@@ -17,9 +15,93 @@ const {
   CALLBACK_URL,
   CONNECTION_STRING,
   SUCCESS_REDIRECT,
-  FAILURE_REDIRECT,
-  REDIRECT_URL
+  FAILURE_REDIRECT
 } = process.env;
 
 const app = express();
 app.use(bodyParser.json());
+
+massive(CONNECTION_STRING)
+  .then(db => {
+    app.set("db", db);
+    console.log("Database Connection Established");
+  })
+  .catch(err => console.log(err));
+
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: true,
+      secret: SESSION_SECRET
+    })
+  );
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  passport.use(
+    new Auth0Strategy(
+      {
+        domain: DOMAIN,
+        clientID: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        callbackURL: CALLBACK_URL,
+        scope: "openid profile email "
+      },
+      function(accessToken, refreshToken, extraParams, profile, done) {
+        const db = app.get("db");
+        db.users_DB.find_user([profile.id]).then(userResult => {
+          if (!userResult[0]) {
+            db.users_DB
+              .create_user([profile.displayName, profile.id, profile.picture])
+              .then(createdUser => {
+                return done(null, createdUser[0].id);
+              })
+              .catch(err => console.log(err));
+          } else {
+            return done(null, userResult[0].id);
+          }
+        });
+      }
+    )
+  );
+  
+  passport.serializeUser((id, done) => {
+    done(null, id);
+  });
+  passport.deserializeUser((id, done) => {
+    app
+      .get("db")
+      .find_session_user([id])
+      .then(loggedInUser => {
+        done(null, loggedInUser[0]);
+      })
+      .catch(err => console.log(err));
+  });
+  
+  app.get("/auth", passport.authenticate("auth0"));
+  app.get(
+    "/auth/callback",
+    passport.authenticate("auth0", {
+      successRedirect: SUCCESS_REDIRECT,
+      failureRedirect: FAILURE_REDIRECT
+    })
+  );
+  
+  app.get("/auth/me", function(req, res) {
+    if (!req.user) {
+      res.sendStatus(401);
+    } else {
+      res.status(200).send(req.user);
+    }
+  });
+
+
+
+  app.get("/auth/logout", (req, res) => {
+    req.logOut();
+    res.redirect(process.env.FAILURE_REDIRECT);
+  });
+
+
+  app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`));
